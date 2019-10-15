1993bf6f6f498db8f85562a2543d0d698eb7a3f2
Thomas Wang <twang@assystemtechnologies.com>
2019-10-15 15:15:43 +0200

    Solved 1. Butin pirates

--- contest/a_loot_sharing/sol/sol_Thomas.py
+++ contest/a_loot_sharing/sol/sol_Thomas.py
@@ -2,26 +2,11 @@
 import math
 
 
-L, N = map(int, input().split())
+b, t, n = map(int, input("#Bicornes, # Tricornes, #Pièces").split())
 
-def riddle(n):
-    digits = '0123456789'
-    result = ''
-    for digit in digits:
-        compte = str(n).count(str(digit))
-        print(compte)
-        if compte >= 1:
-            result = result + str(compte) + str(digit)
-    return result
-
-# while(True):
-#     a = input()
-#     print(riddle(a))
-
-def riddle_mutliple(n,p):
-    if p == 1:
-        return riddle(n)
-    else:
-        return riddle( riddle_mutliple(n, p-1) )
-
-print( riddle_mutliple(L, N) )
+to_distribute = 2*b + 3*t
+if to_distribute <= n:
+    result = 'Loot!'
+else:
+    result = 'Rhum!'
+print(result)
